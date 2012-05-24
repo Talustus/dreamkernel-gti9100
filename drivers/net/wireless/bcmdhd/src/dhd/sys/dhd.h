d38699a55d2cd8b741de2a9d76fd78a907664fd4
Talustus <talustus@dream-irc.de>
2012-05-24 17:53:13 +0200

    net: wireless: bcmdhd: n7000 update3 wifi driver

--- drivers/net/wireless/bcmdhd/src/dhd/sys/dhd.h
+++ drivers/net/wireless/bcmdhd/src/dhd/sys/dhd.h
@@ -34,6 +34,7 @@
 #ifndef _dhd_h_
 #define _dhd_h_
 
+#include "dhd_sec_feature.h"
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/slab.h>
@@ -252,6 +253,7 @@ typedef struct dhd_pub {
 	void* wlfc_state;
 #endif
 	bool	dongle_isolation;
+	bool	dongle_trap_occured;	/* flag for forcible sending HANG event whenever trap occured */
 	int   hang_was_sent;
 	int   rxcnt_timeout;		/* counter rxcnt timeout to send HANG */
 	int   txcnt_timeout;		/* counter txcnt timeout to send HANG */
@@ -259,6 +261,9 @@ typedef struct dhd_pub {
 	uint8 htsfdlystat_sz; /* Size of delay stats, max 255B */
 #endif
 	struct reorder_info *reorder_bufs[WLHOST_REORDERDATA_MAXFLOWS];
+#if defined(PNO_SUPPORT) && defined(CONFIG_HAS_WAKELOCK)
+	struct wake_lock	pno_wakelock;
+#endif
 } dhd_pub_t;
 
 	#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)) && defined(CONFIG_PM_SLEEP)
@@ -346,25 +351,25 @@ inline static void MUTEX_UNLOCK_SOFTAP_SET(dhd_pub_t * dhdp)
 		printf("call wake_lock: %s %d\n", \
 			__FUNCTION__, __LINE__); \
 		dhd_os_wake_lock(pub); \
-	} while(0)
+	} while (0)
 #define DHD_OS_WAKE_UNLOCK(pub) \
 	do { \
 		printf("call wake_unlock: %s %d\n", \
 			__FUNCTION__, __LINE__); \
 		dhd_os_wake_unlock(pub); \
-	} while(0)
+	} while (0)
 #define DHD_OS_WAKE_LOCK_TIMEOUT(pub) \
 	do { \
 		printf("call wake_lock_timeout: %s %d\n", \
 			__FUNCTION__, __LINE__); \
 		dhd_os_wake_lock_timeout(pub); \
-	} while(0)
+	} while (0)
 #define DHD_OS_WAKE_LOCK_TIMEOUT_ENABLE(pub, val) \
 	do { \
 		printf("call wake_lock_timeout_enable[%d]: %s %d\n", \
 			val, __FUNCTION__, __LINE__); \
 		dhd_os_wake_lock_timeout_enable(pub, val); \
-	} while(0)
+	} while (0)
 #else
 #define DHD_OS_WAKE_LOCK(pub)			dhd_os_wake_lock(pub)
 #define DHD_OS_WAKE_UNLOCK(pub)			dhd_os_wake_unlock(pub)
@@ -554,7 +559,7 @@ extern uint dhd_bus_status(dhd_pub_t *dhdp);
 extern int  dhd_bus_start(dhd_pub_t *dhdp);
 extern int dhd_bus_membytes(dhd_pub_t *dhdp, bool set, uint32 address, uint8 *data, uint size);
 extern void dhd_print_buf(void *pbuf, int len, int bytes_per_line);
-extern bool dhd_is_associated(dhd_pub_t *dhd, void *bss_buf, int *retval);
+extern bool dhd_is_associated(dhd_pub_t *dhd, void *bss_buf);
 
 #if defined(KEEP_ALIVE)
 extern int dhd_keep_alive_onoff(dhd_pub_t *dhd);
@@ -656,8 +661,13 @@ extern char fw_path2[MOD_PARAM_PATHLEN];
 /* Flag to indicate if we should download firmware on driver load */
 extern uint dhd_download_fw_on_driverload;
 
-/* Flags to indicate if we distingish power off scheme during suspend */
-extern bool suspend_power_off;
+#if defined(WL_CFG80211) && defined(CUSTOMER_HW_SAMSUNG)
+/* CSP#505233: Flags to indicate if we distingish power off policy when
+ * user set the memu "Keep Wi-Fi on during sleep" to "Never"
+ */
+extern int sleep_never;
+int dhd_deepsleep(struct net_device *dev, int flag);
+#endif /* WL_CFG80211 && CUSTOMER_HW_SAMSUNG */
 
 #ifdef BCM4334_CHECK_CHIP_REV
 /* Check chip revision */
