8b63f635f6a5afe38e82bc908fe30198fc97ab8e
Talustus <talustus@dream-irc.de>
2012-06-25 20:05:27 +0200

    2.4 build-script

--- build_kernel.sh
+++ build_kernel.sh
@@ -31,7 +31,7 @@ export RELEASEDIR=`readlink -f $KERNELDIR/../releases`
 #
 # Version of this Build
 #
-KRNRLS="DreamKernel-v2.3"
+KRNRLS="DreamKernel-v2.4"
 KBUILD_BUILD_HOST=`hostname | sed 's|ip-projects.de|dream-irc.com|g'`
 HOSTNAME=$KBUILD_BUILD_HOST
 #
