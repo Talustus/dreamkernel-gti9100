161a881127ee39fdccf4e637ce08dbd22712772a
Talustus <talustus@dream-irc.de>
2012-07-13 03:36:54 +0200

    Version 3.2.2TWRP

--- build_kernel-twrp.sh
+++ build_kernel-twrp.sh
@@ -31,7 +31,7 @@ export RELEASEDIR=`readlink -f $KERNELDIR/../releases`
 #
 # Version of this Build
 #
-KRNRLS="DreamKernel-v3.0_TWRP"
+KRNRLS="DreamKernel-v3.2.2TWRP"
 KBUILD_BUILD_HOST=`hostname | sed 's|ip-projects.de|dream-irc.com|g'`
 HOSTNAME=$KBUILD_BUILD_HOST
 #
