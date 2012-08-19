e8dcd6d2f0db665cc7f0347742ba52a8462104a7
Andrew Dodd <atd7@cornell.edu>
2012-08-19 15:39:28 -0400

    u1-gpio: Add GPIO definitions for SGH-I777
    Conflicts:
    
    	arch/arm/mach-exynos/u1-gpio.c
    
    Change-Id: I75aee5c4b67975056998e91be99ca70ff357247b

--- arch/arm/mach-exynos/u1-gpio.c
+++ arch/arm/mach-exynos/u1-gpio.c
@@ -211,7 +211,7 @@ static struct gpio_init_data u1_init_gpios[] = {
 		S3C_GPIO_PULL_NONE, S5P_GPIO_DRVSTR_LV1},
 #endif /*CONFIG_TARGET_LOCALE_NA*/
 
-#if defined(CONFIG_TARGET_LOCALE_NA)
+#if defined(CONFIG_TARGET_LOCALE_NA) || defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
 	/* NC*/
 	{EXYNOS4_GPX3(5), S3C_GPIO_INPUT, S3C_GPIO_SETPIN_ZERO,
 		S3C_GPIO_PULL_DOWN, S5P_GPIO_DRVSTR_LV1},
@@ -476,7 +476,12 @@ static unsigned int u1_sleep_gpio_table[][3] = {
 	{EXYNOS4_GPB(4), S3C_GPIO_SLP_INPUT, S3C_GPIO_PULL_DOWN},
 	{EXYNOS4_GPB(5), S3C_GPIO_SLP_INPUT, S3C_GPIO_PULL_DOWN},
 #else
+#if defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
+	{EXYNOS4_GPB(0), S3C_GPIO_SLP_INPUT,  S3C_GPIO_PULL_NONE},
+#else
 	{EXYNOS4_GPB(0), S3C_GPIO_SLP_PREV,  S3C_GPIO_PULL_NONE},
+#endif
+
 #if defined(CONFIG_LEDS_GPIO)
 	{EXYNOS4_GPB(1), S3C_GPIO_SLP_OUT0, S3C_GPIO_PULL_NONE},
 #else /*CONFIG_LEDS_GPIO*/
@@ -1088,6 +1093,14 @@ static unsigned int u1_sleep_gpio_table[][3] = {
 #endif
 };
 
+#if defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
+static unsigned int u1_exint_sleep_gpio_table[][3] = {
+	{ EXYNOS4_GPX2(4),  S3C_GPIO_SLP_INPUT, S3C_GPIO_PULL_DOWN},
+	{ EXYNOS4_GPX3(0),  S3C_GPIO_SLP_INPUT, S3C_GPIO_PULL_DOWN},
+	{ EXYNOS4_GPX3(5),  S3C_GPIO_SLP_INPUT, S3C_GPIO_PULL_DOWN},
+};
+#endif
+
 void u1_config_gpio_table(void)
 {
 	u32 i, gpio;
@@ -1116,8 +1129,26 @@ static void config_sleep_gpio_table(int array_size,
 	}
 }
 
+#if defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
+static void config_exint_sleep_gpio_table(int array_size,
+				unsigned int (*gpio_table)[3])
+{
+	u32 i, gpio;
+
+	for (i = 0; i < ARRAY_SIZE(u1_exint_sleep_gpio_table); i++) {
+		gpio = u1_exint_sleep_gpio_table[i][0];
+		s3c_gpio_cfgpin(gpio, u1_exint_sleep_gpio_table[i][1]);
+		s3c_gpio_setpull(gpio, u1_exint_sleep_gpio_table[i][2]);
+	}
+}
+#endif
+
 void u1_config_sleep_gpio_table(void)
 {
 	config_sleep_gpio_table(ARRAY_SIZE(u1_sleep_gpio_table),
 			u1_sleep_gpio_table);
+#if defined(CONFIG_TARGET_LOCALE_NAATT_TEMP)
+	config_exint_sleep_gpio_table(ARRAY_SIZE(u1_exint_sleep_gpio_table),
+			u1_exint_sleep_gpio_table);
+#endif
 }
