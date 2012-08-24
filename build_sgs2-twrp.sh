9b887a6589d89f0850ffa7e88fb72ab4ae26372b
Talustus <talustus@dream-irc.de>
2012-08-24 22:59:05 +0200

    Config 2.7.2/3.3.2 Changes

--- build_sgs2-twrp.sh
+++ build_sgs2-twrp.sh
@@ -31,7 +31,7 @@ export RELEASEDIR=`readlink -f $KERNELDIR/../releases`
 #
 # Version of this Build
 #
-KRNRLS="DreamKernel-v3.3.1TWRP"
+KRNRLS="DreamKernel-v3.3.2TWRP"
 KBUILD_BUILD_HOST=`hostname | sed 's|ip-projects.de|dream-irc.com|g'`
 HOSTNAME=$KBUILD_BUILD_HOST
 #
