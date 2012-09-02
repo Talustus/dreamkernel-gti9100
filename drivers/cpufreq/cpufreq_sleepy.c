deaf8e7db5b507c735ada89024fa82cce7229a26
Talustus <talustus@dream-irc.de>
2012-09-02 13:10:23 +0200

    Sleepy: Remove X86 related Code

--- drivers/cpufreq/cpufreq_sleepy.c
+++ drivers/cpufreq/cpufreq_sleepy.c
@@ -805,15 +805,6 @@ static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)
  */
 static int should_io_be_busy(void)
 {
-#if defined(CONFIG_X86)
-        /*
-         * For Intel, Core 2 (model 15) andl later have an efficient idle.
-         */
-        if (boot_cpu_data.x86_vendor == X86_VENDOR_INTEL &&
-            boot_cpu_data.x86 == 6 &&
-            boot_cpu_data.x86_model >= 15)
-                return 1;
-#endif
         return 0;
 }
 
